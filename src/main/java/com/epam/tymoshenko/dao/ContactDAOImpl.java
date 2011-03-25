ab65db005364599321bb4a7b5a6131f71fa42bbd
Alphard16 <Alphard16@gmail.com>
2011-03-25 18:14:24 +0000

    initialization

---
+++ src/main/java/com/epam/tymoshenko/dao/ContactDAOImpl.java
@@ -0,0 +1,36 @@
+package com.epam.tymoshenko.dao;
+
+import java.util.List;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.epam.tymoshenko.domain.Contact;
+
+@Repository
+public class ContactDAOImpl implements ContactDAO {
+
+	@Autowired
+	private SessionFactory sessionFactory;
+
+	public void addContact(Contact contact) {
+		sessionFactory.getCurrentSession().save(contact);
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<Contact> listContact() {
+
+		return sessionFactory.getCurrentSession().createQuery("from Contact")
+			.list();
+	}
+
+	public void removeContact(Integer id) {
+		Contact contact = (Contact) sessionFactory.getCurrentSession().load(
+				Contact.class, id);
+		if (null != contact) {
+			sessionFactory.getCurrentSession().delete(contact);
+		}
+
+	}
+}
---
+++ src/main/java/com/epam/tymoshenko/service/ContactServiceImpl.java
@@ -0,0 +1,34 @@
+package com.epam.tymoshenko.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.epam.tymoshenko.dao.ContactDAO;
+import com.epam.tymoshenko.domain.Contact;
+
+@Service
+public class ContactServiceImpl implements ContactService{
+
+    @Autowired
+    private ContactDAO contactDAO;
+ 
+    @Transactional
+    public void addContact(Contact contact) {
+        contactDAO.addContact(contact);
+    }
+ 
+    @Transactional
+    public List<Contact> listContact() {
+ 
+        return contactDAO.listContact();
+    }
+ 
+    @Transactional
+    public void removeContact(Integer id) {
+        contactDAO.removeContact(id);
+    }	
+
+}
